83147e79e1f59df7621193a19ad55897d151c552
branislav985 <branislav985@gmail.com>
2024-01-25 22:37:16 +0100

    first run ok

---
+++ tests/Sesion2/firstTest.js
@@ -0,0 +1,6 @@
+fixture`firstTest`.page`https://demoqa.com/text-box`.skipJsErrors();
+
+test.page`demoqa.com/text-box`("Test1", async (t) => {
+  console.log("demo Qa Site");
+  // Starts at http://devexpress.github.io/testcafe/example
+});
